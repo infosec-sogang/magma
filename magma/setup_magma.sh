e8b84df526e26f8ae88acbd33eace18f561dda7c
choiGun <choigeon0513@gmail.com>
2024-05-22 02:23:00 +0900

    Create setup_magma.sh

---
+++ magma/setup_magma.sh
@@ -0,0 +1,19 @@
+export MAGMA_R=/magma
+export MAGMA=/magma/magma
+export OUT=/magma_out
+export SHARED=/magma_shared
+
+export CC=/usr/bin/gcc
+export CXX=/usr/bin/g++
+export LD=/usr/bin/ld
+export AR=/usr/bin/ar
+export AS=/usr/bin/as
+export NM=/usr/bin/nm
+export RANLIB=/usr/bin/ranlib
+
+BUILD_FLAGS="-include ${MAGMA}/src/canary.h -DMAGMA_ENABLE_CANARIES -DMAGMA_FATAL_CANARIES -g -O0"
+
+export CFLAGS=${BUILD_FLAGS}
+export CXXFLAGS=${BUILD_FLAGS}
+export LIBS="-l:magma.o -lrt"
+export LDFLAGS="-L${OUT} -g"

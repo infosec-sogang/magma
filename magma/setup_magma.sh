db051e958b3df5b0dbb5ae4f52a3f8c9ac13e06d
Jaeseung Choi <jschoi.2022@gmail.com>
2024-06-25 23:48:32 +0900

    Fix setup_magma to not overwrite important environment variables

--- magma/setup_magma.sh
+++ magma/setup_magma.sh
@@ -5,8 +5,6 @@ export MAGMA=/magma/magma
 export OUT=/magma_out
 export SHARED=/magma_shared
 
-export CC=/usr/bin/gcc
-export CXX=/usr/bin/g++
 export LD=/usr/bin/ld
 export AR=/usr/bin/ar
 export AS=/usr/bin/as
@@ -15,7 +13,7 @@ export RANLIB=/usr/bin/ranlib
 
 BUILD_FLAGS="-include ${MAGMA}/src/canary.h -DMAGMA_ENABLE_CANARIES -DMAGMA_FATAL_CANARIES -g -O0"
 
-export CFLAGS=${BUILD_FLAGS}
-export CXXFLAGS=${BUILD_FLAGS}
+export CFLAGS="$CFLAGS $BUILD_FLAGS"
+export CXXFLAGS="$CXXFLAGS $BUILD_FLAGS"
 export LIBS="-l:magma.o -lrt"
 export LDFLAGS="-L${OUT} -g"

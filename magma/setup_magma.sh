b9130b623be9c0e4d9c8f0e2fb95524a756a3f59
Jaeseung Choi <jschoi.2022@gmail.com>
2024-11-24 17:59:42 +0900

    Refactor the structure of setup_magma.sh script
    Choose a design that can avoid unexpected side-effects.

--- magma/setup_magma.sh
+++ magma/setup_magma.sh
@@ -1,30 +1,22 @@
 # Imported from the original Dockerfile in the magma repository
-export MAGMA_R=/magma
-export MAGMA=/magma/magma
-export OUT=/magma_out
-export SHARED=/magma_shared
-
-export LD=/usr/bin/ld
-export AR=/usr/bin/ar
-export AS=/usr/bin/as
-export NM=/usr/bin/nm
-export RANLIB=/usr/bin/ranlib
-
-BUILD_FLAGS="-include ${MAGMA}/src/canary.h -DMAGMA_ENABLE_CANARIES -DMAGMA_FATAL_CANARIES -g -O0"
-
-export CFLAGS=$BUILD_FLAGS
-export CXXFLAGS=$BUILD_FLAGS
-export LIBS="-l:magma.o -l:afl_driver.o -lrt"
-export LDFLAGS="-L${OUT} -g"
+mkdir -p /magma_out
+MAGMA=/magma/magma # Magma's base directory must be placed at this path
+OUT=/magma_out
+MAGMA_CFLAGS="-include ${MAGMA}/src/canary.h -DMAGMA_ENABLE_CANARIES -DMAGMA_FATAL_CANARIES -g -O0"
+MAGMA_LDFLAGS="-L${OUT} -g"
+MAGMA_LIBS="-l:magma.o -l:afl_driver.o -lrt"
 
 # Prepare common object files in the output directory. While the original Magma
 # repository uses fuzzer-specific $CC and $CFLAGS to build these binaries, it
 # won't hurt to compile these with a common compiler (clang) and flags.
-rm -rf $OUT/*
-clang -c "$MAGMA/src/afl_driver.c" -fPIC -o $OUT/afl_driver.o
-clang $CFLAGS -D"MAGMA_STORAGE=\"$MAGMA_STORAGE\"" -c "$MAGMA/src/canary.c" \
-    -fPIC -I "$MAGMA/src/" -o "$OUT/canary.o" $LDFLAGS
-clang $CFLAGS -D"MAGMA_STORAGE=\"$MAGMA_STORAGE\"" -c "$MAGMA/src/storage.c" \
-    -fPIC -I "$MAGMA/src/" -o "$OUT/storage.o" $LDFLAGS
-ld -r "$OUT/canary.o" "$OUT/storage.o" -o "$OUT/magma.o"
-rm "$OUT/canary.o" "$OUT/storage.o"
+function setup_magma() {
+  rm -rf $OUT/*
+  export CFLAGS=$CFLAGS_ARG
+  $CC -c "$MAGMA/src/afl_driver.c" -fPIC -o $OUT/afl_driver.o
+  $CC $CFLAGS -D"MAGMA_STORAGE=\"\"" -c "$MAGMA/src/canary.c" \
+      -fPIC -I "$MAGMA/src/" -o "$OUT/canary.o" $LDFLAGS
+  $CC $CFLAGS -D"MAGMA_STORAGE=\"\"" -c "$MAGMA/src/storage.c" \
+      -fPIC -I "$MAGMA/src/" -o "$OUT/storage.o" $LDFLAGS
+  $LD -r "$OUT/canary.o" "$OUT/storage.o" -o "$OUT/magma.o"
+  rm "$OUT/canary.o" "$OUT/storage.o"
+}
